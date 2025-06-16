a2ce227c0a16740cb35f026fe951833462d31105
OwDevon <d.legaleri@gmail.com>
2025-06-16 19:33:06 +0200

    Ton message de commit ici

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -2,6 +2,7 @@ import { projects } from '@/data'
 import React from 'react'
 import { PinContainer } from "./ui/Pin";
 import { FaLocationArrow } from 'react-icons/fa6'
+import Image from 'next/image';
 
 const RecentProjects = () => {
   const handleClick = (link: string) => {
@@ -30,7 +31,7 @@ const RecentProjects = () => {
             <PinContainer title={link} href={link}>
               <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10'>
                 <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
-                  <img src="/bg.png" alt="bg-img" />
+                  <Image src="/bg.png" alt="bg-img" />
                 </div>
                 <img src={img} alt={title} className='z-10 absolute bottom-0' />
               </div>
